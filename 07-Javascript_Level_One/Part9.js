6df36b5784ae0b366279ac5d0e428896c60caf27
Runal <runalb@users.noreply.github.com>
2021-05-19 22:34:47 +0530

    Javascript_L1-Project

--- 07-Javascript_Level_One/Part9.js
+++ 07-Javascript_Level_One/Part9.js
@@ -1,8 +1,8 @@
-firstName = prompt("Hello and Welcome. Please enter your first name:")
-lastName = prompt("Please enter your Last Name:")
-age = prompt("How old are you?")
-height = prompt("How tall are you in centimeters?")
-petName = prompt("What is the name of your pet?")
+var firstName = prompt("Hello and Welcome. Please enter your first name:")
+var lastName = prompt("Please enter your Last Name:")
+var age = prompt("How old are you?")
+var height = prompt("How tall are you in centimeters?")
+var petName = prompt("What is the name of your pet?")
 alert("Thank you so much for the information.")
 
 var condition1 = null
