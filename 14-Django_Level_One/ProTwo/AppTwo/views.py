9a1a2ac6f4f194ab7cebc6858e3f471beae778ae
runalb <65166862+runalb@users.noreply.github.com>
2022-06-29 19:18:09 +0530

    124 - Templates Challenge Solution

--- 14-Django_Level_One/ProTwo/AppTwo/views.py
+++ 14-Django_Level_One/ProTwo/AppTwo/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
+def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    helpdict = {'help_insert':'Help Page from AppTwo.views'}
+    return render(request,'AppTwo/help.html', context=helpdict)
